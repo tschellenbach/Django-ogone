63b86daa3368fa0eef2ea1855b8edaaa74700505
Mathijs de Bruin <drbob@dokterbob.net>
2010-09-23 17:07:10 +0200

    Unknown code should simply raise an exception

--- django_ogone/exceptions.py
+++ django_ogone/exceptions.py
@@ -4,3 +4,13 @@ class InvalidSignatureException(Exception):
     
 class InvalidParamsException(Exception):
     pass
+
+class UnknownStatusException(Exception):
+   def __init__(self, status):
+       self.status = status
+   
+   def __unicode__(self):
+       return u'Ogone returned an unkown status code \'%s\'' % self.status
+
+   def __str__(self):
+       return repr(self.parameter)
