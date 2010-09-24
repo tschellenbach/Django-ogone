919fde5918a35f021a23c8716af870914692d407
Mathijs de Bruin <drbob@dokterbob.net>
2010-09-24 21:27:52 +0200

    Brand new settings.py

--- setup.py
+++ setup.py
@@ -1,40 +1,39 @@
 #!/usr/bin/env python
 
-import os
-from distutils.core import setup
 from django_ogone import __version__, __maintainer__, __email__
+from setuptools import setup, find_packages
 
+try:
+    README = open('README.md').read()
+except:
+    README = None
 
-license_text = open('LICENSE.txt').read()
-long_description = open('README.md').read()
-
-CLASSIFIERS = [
-                'Development Status :: 5 - Production/Stable',
-                'Intended Audience :: Developers',
-                'Framework :: Django',
-                'License :: OSI Approved :: GNU General Public License (GPL)',
-                'Natural Language :: English',
-                'Operating System :: OS Independent',
-                'Programming Language :: Python',
-                'Topic :: Scientific/Engineering :: Mathematics',
-                'Topic :: Software Development :: Libraries :: Python Modules',
-                'Environment :: Web Environment',
-              ]
-
-DESCRIPTION = """Python/Django client to the ogone payment system. Aids in setting up secure payment functionality."""
+try: 
+    LICENSE = open('LICENSE.txt').read() 
+except: 
+    LICENSE = None
 
 
 setup(
     name = 'django-ogone',
     version = __version__,
-    url = 'http://github.com/tschellenbach/Django-ogone',
+    description='Python/Django client to the ogone payment system.',
+    long_description=README,
     author = __maintainer__,
     author_email = __email__,
-    license = license_text,
-    packages = ['django_ogone'],
-    data_files=[('', ['LICENSE.txt',
-                      'README.rst'])],
-    description = DESCRIPTION,
-    long_description=long_description,
-    classifiers = CLASSIFIERS
+    license = LICENSE,
+    url = 'http://github.com/tschellenbach/Django-ogone',
+    packages = find_packages(),
+    data_files=('',['README.md', 'LICENSE.txt']),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Environment :: Web Environment',
+    ],
 )
