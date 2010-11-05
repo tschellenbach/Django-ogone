c4a96ce64f5950e8322fb522e55fd865f7707e89
zyegfryed <zyegfryed@gmail.com>
2010-11-05 19:17:15 +0100

    Fixed missing import.

--- django_ogone/__init__.py
+++ django_ogone/__init__.py
@@ -4,7 +4,7 @@ __email__ = 'thierryschellenbach at googles great mail service'
 
 # Be careful here, otherwise setup.py won't work
 try:
-    from django_ogone.ogone import Ogone
+    from django_ogone.ogone import Ogone, OgoneDirectLink
     __ALL__ = (Ogone, OgoneDirectLink)
 except ImportError:
     pass
