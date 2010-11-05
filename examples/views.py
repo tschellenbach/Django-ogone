7045baf285fd91a9ade3a041b7cf684ca06e8bc6
zyegfryed <zyegfryed@gmail.com>
2010-11-05 11:36:16 +0100

    Added an example to handle accented characters from Ogone.

--- examples/views.py
+++ examples/views.py
@@ -1,11 +1,14 @@
-
-from django_ogone.ogone import Ogone
-from django_ogone.forms import ogone_forms        
-from django_ogone import ogone_settings        
-'''
+"""
 The implementation of these examples differs strongly per site
 Understand the example and consult the ogone docs to roll your own
-'''
+"""
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+
+from django_ogone.ogone import Ogone
+from django_ogone.forms import ogone_forms
+from django_ogone import ogone_settings
 
 
 def checkout(request):
@@ -18,16 +21,10 @@ def checkout(request):
     data['SHASign'] = Ogone.sign(data)
     
     context = {}
-    context['form'] = ogone_forms.OgoneForm(data)
+    context['form'] = Ogone.get_form(data)
+    context['action'] = Ogone.get_action()
     
-    if ogone_settings.PRODUCTION:
-        request.context['action'] = 'https://secure.ogone.com/ncol/test/orderstandard.asp'
-    else:
-        request.context['action'] = 'https://secure.ogone.com/ncol/prod/orderstandard.asp'
-    
-
-
-
+    return render_to_response('shop/checkout/form.html', context)
 
 
 def order_status_update(request):
@@ -42,7 +39,11 @@ def order_status_update(request):
     For testing order flow updates
     http://localhost:8080/order_status_update/?orderID=12&currency=EUR&amount=680.44&PM=CreditCard&ACCEPTANCE=test123&STATUS=5&CARDNO=XXXXXXXXXXXX1111&ED=0114&CN=thierry&TRXDATE=09/21/10&PAYID=8254874&NCERROR=0&BRAND=VISA&IPCTY=NL&CCCTY=US&ECI=12&CVCCheck=NO&AAVCheck=NO&VC=NO&IP=85.145.6.230&SHASIGN=B02C883D4D31D9665305EA05CC81D3DED7726C68F18C870A8C8F3119DC1B460D9103944692B6E44D47EEA402630770A8122F6D1B7028CC5FD58847DC43D7C082
     '''
-    params = request.POST or request.GET
+    # Workaround because Ogone only deals with latin1 characters and we want
+    # our wbeite to use another ``DEFAULT_CHARSET`` (default: utf8).
+    from django.http import QueryDict
+    params = QueryDict(request.META['QUERY_STRING'], encoding='latin1')
+    # --//--
     ogone = Ogone(params)
     
     if ogone.is_valid():
@@ -52,7 +53,7 @@ def order_status_update(request):
         
         #redirect to the appropriate view
         order_id = ogone.get_order_id()
-        url = '%s?transaction_id=%s' % (reverse('checkout'), order_id)
+        url = reverse('tracking', args=[order_id])
         
         return HttpResponseRedirect(url)
 
